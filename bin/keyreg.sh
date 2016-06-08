30714e67f34a9494b09af0f67ffea97f7720ec03
korbenk <korben@kirscht.com>
2016-06-07 21:02:03 -0700

    Ping script.

---
+++ bin/keyreg.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+ssh-agent > ${HOME}/ssh_agent
+
+. ${HOME}/ssh_agent
+
+ssh-add ${HOME}/.ssh/ansible_ssh_key
+
+cat ${HOME}/.ssh/ansible_ssh_key >> ${HOME}/.ssh/authorized_keys
