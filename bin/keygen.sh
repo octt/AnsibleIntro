4d8bc3410245378b4f0a2a2f9e10bbb522edad9d
korbenk <korben@kirscht.com>
2016-06-07 20:43:35 -0700

    Ping script.

--- bin/keygen.sh
+++ bin/keygen.sh
@@ -3,7 +3,7 @@
 export _SSH_DIR=${HOME}/.ssh/
 [ ! -d ${_SSH_DIR} ] mkdir -p ${_SSH_DIR}
 
-chmod 0400 ${_SSH_DIR}
+chmod 0700 ${_SSH_DIR}
 
 ssh-keygen -B -b 4096 -C "Ansible Intro" -f ${_SSH_DIR}ansible_ssh_key \
            -N AnsibleIntro
